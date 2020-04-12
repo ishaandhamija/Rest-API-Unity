b429958ee192cb0868b4c3fb28471499345d7941
Ishaan Dhamija <ishaandhamija@DV-LT-N-019111.local>
2020-04-12 14:03:54 +0530

    initial commit

---
+++ Assets/Diamond.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Diamond
+{
+	// public string playerName;
+	// public List<ballList> balls;
+	public string data;
+}
+
+[Serializable]
+public class ballList
+{
+	public string name;
+}
---
+++ Assets/DiamondScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour
+{
+
+	public string jsonURL;
+
+    void Start()
+    {
+    	StartCoroutine(getData());
+    }
+
+    IEnumerator getData() {
+    	Debug.Log("Processing Data..");
+
+    	WWW _www = new WWW(jsonURL);
+    	yield return _www;
+
+    	if (_www.error == null) {
+    		processJsonData(_www.text);
+    	} else {
+    		Debug.Log("Networking Dikkat hai");
+    	}
+    }
+
+    private void processJsonData(string _url) {
+    	Diamond jsnData = JsonUtility.FromJson<Diamond>(_url);
+    	// Debug.Log(jsnData.playerName);
+    	// Debug.Log(jsnData.balls);
+    	Debug.Log(jsnData.data);
+    }
+}
